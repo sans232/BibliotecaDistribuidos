1a8a1b12f2368b362e2ea629ad65628dc3ec47f3
sans232 <sansypapyrus95@gmail.com>
2024-05-07 09:20:24 -0400

    BibliotecaBase

---
+++ ProyectoBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=sql.bsite.net\\MSSQL2016;Initial Catalog=sistdist_SampleDB;User ID=sistdist_SampleDB;Password=sistemas123;";
+    }
+}
