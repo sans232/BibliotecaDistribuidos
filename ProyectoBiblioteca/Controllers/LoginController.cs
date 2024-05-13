abe76f27e74a8cabb5c5a60de0ce26e3c3cf39d7
Brayankevin <49537064+Brayankevin@users.noreply.github.com>
2024-05-13 18:51:33 -0400

    Update LoginController.cs

--- ProyectoBiblioteca/Controllers/LoginController.cs
+++ ProyectoBiblioteca/Controllers/LoginController.cs
@@ -3,8 +3,6 @@ using ProyectoBiblioteca.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,24 +10,6 @@ namespace ProyectoBiblioteca.Controllers
 {
     public class LoginController : Controller
     {
-        //hasheartr contraseñas
-        private string HashPassword(string password)
-        {
-            using (SHA256 sha256Hash = SHA256.Create())
-            {
-                // Compute hash
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
-
-                // Convert byte array to a string
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
-            }
-        }
-
         // GET: Login
         public ActionResult Index()
         {
@@ -39,21 +19,16 @@ namespace ProyectoBiblioteca.Controllers
         [HttpPost]
         public ActionResult Index(string correo, string clave)
         {
-            // Hash contraseña ingresada por el usuario
-            string hashedPassword = HashPassword(clave);
 
-            // Buscar al usuario en la base de datos usando el correo y la contraseña hasheada
-            Persona usuario = PersonaLogica.Instancia.Listar()
-                .Where(u => u.Correo == correo && u.Clave == hashedPassword && u.oTipoPersona.IdTipoPersona != 3)
-                .FirstOrDefault();
+            Persona ousuario = PersonaLogica.Instancia.Listar().Where(u => u.Correo == correo && u.Clave == clave && u.oTipoPersona.IdTipoPersona != 3).FirstOrDefault();
 
-            if (usuario == null)
+            if (ousuario == null)
             {
                 ViewBag.Error = "Usuario o contraseña no correcta";
                 return View();
             }
 
-            Session["Usuario"] = usuario;
+            Session["Usuario"] = ousuario;
 
             return RedirectToAction("Index", "Admin");
         }
